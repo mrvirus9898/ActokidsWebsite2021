2a97e4b6cc28de70e8406bcd5bc0105fd78b72a7
Nick Bennett <jrtngb@yahoo.com>
2021-03-21 15:15:44 -0700

    Changed the row order of some components in Program Details, added debug descriptions.

--- screens/ProgramDetails.tsx
+++ screens/ProgramDetails.tsx
@@ -16,7 +16,7 @@ Add Calendar Intent
 Add ios Intents
 */
 
-import React, {Component} from 'react';
+import React from 'react';
 import {
   StyleSheet,
   Text,
@@ -61,15 +61,10 @@ export default function ProgramDetails(params: Array<any>){
     return(
         <View style={styles.container}>
         <ScrollView>
-          <Image style={{ width: 390, height: 200 }} source={{ uri: item.Program_Image_Url}} />
-
             <View style={styles.row}>
               <View style={styles.textWrap}>
                 <Text style={styles.ProgramName}>{item.Program_Name}</Text>
-              </View>
-            </View>
-
-            <TouchableHighlight
+                <TouchableHighlight
               onPress= {() => {
                 alert('Pass Calander Intent')
               }}>
@@ -113,6 +108,11 @@ export default function ProgramDetails(params: Array<any>){
                 </View>
               </View>
             </TouchableHighlight>
+              </View>
+              <Image style={{ width: 390, height: 200 }} source={{ uri: item.Program_Image_Url}} />
+            </View>
+
+
             <View style={{ borderColor: 'lightgray', borderBottomWidth: 2, marginTop: 30, marginBottom: 20 }}></View>
             <Text style={styles.subTitles}>DESCRIPTION</Text>
             <Text style={styles.bodyText}>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</Text>
